78e0da87059b91f488f12c0a656f192515ad81f2
digbose92 <digbose92@gmail.com>
2019-04-01 18:07:06 -0700

    Added the model training

--- models.py
+++ models.py
@@ -15,7 +15,6 @@ def create_embedding_layer(weights_matrix,non_trainable=False):
     emb_layer = nn.Embedding(num_embeddings, embedding_dim)
     print(weights_matrix.shape)
     emb_layer.weight.data.copy_(torch.from_numpy(weights_matrix))
-    #emb_layer.load_state_dict({'weight': weights_matrix})
     if non_trainable:
         emb_layer.weight.requires_grad = False
 
@@ -23,7 +22,7 @@ def create_embedding_layer(weights_matrix,non_trainable=False):
 
 
 class EncoderLSTM(nn.Module):
-    def __init__(self, hidden_size, vocab_size, weights_matrix,train_embed=False,use_gpu=True,fc_size=2048, num_layers=2, max_seq_length=14, batch_size=32):
+    def __init__(self, hidden_size,weights_matrix,train_embed=False,use_gpu=True,fc_size=2048, num_layers=2, max_seq_length=14, batch_size=32):
         """Module for stacked LSTM which returns a single hidden state for the input question
         """
         super(EncoderLSTM, self).__init__()
@@ -62,17 +61,17 @@ class EncoderLSTM(nn.Module):
         return(act_vals)
 
 class FusionModule(nn.Module):
-    def __init__(self,input_size=2048,fuse_embed_size=1024,class_size=3125):
+    def __init__(self,fuse_embed_size=2048,fc_size=1024,class_size=3125):
         """Module for fusing the mean pooled image features and lstm hidden states
         """
         super(FusionModule, self).__init__()
         self.fuse_size=fuse_embed_size
         self.num_classes=class_size
-        self.inp_emb_size=input_size
+        self.fc_size=fc_size
 
         
-        self.embed_layer=nn.Linear(self.fuse_size,self.inp_emb_size)
-        self.class_layer=nn.Linear(self.inp_emb_size,self.num_classes)
+        self.embed_layer=nn.Linear(self.fuse_size,self.fc_size)
+        self.class_layer=nn.Linear(self.fc_size,self.num_classes)
 
 
     def forward(self,encoder_hidden_states, image_features):
@@ -85,6 +84,7 @@ class FusionModule(nn.Module):
         class_vals=F.softmax(class_embed,dim=1)
         return(class_vals)
 
+
 if __name__ == "__main__":
 
         weights_file="data/glove6b_init_300d.npy"
@@ -96,16 +96,17 @@ if __name__ == "__main__":
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         #testing the EncoderLSTM part
-        encode_test=EncoderLSTM(hidden_size=3,weights_matrix=embed_weights,vocab_size=5,use_gpu=True,fc_size=3,max_seq_length=3,batch_size=2)
+        encode_test=EncoderLSTM(hidden_size=3,weights_matrix=embed_weights,use_gpu=True,fc_size=3,max_seq_length=3,batch_size=2)
         encode_test=encode_test.to(device)
         test_torch_tensor=test_torch_tensor.to(device)
         test_image_tensor=test_image_feats.to(device)
-        encoder_ops=encode_test.forward(test_torch_tensor)
-        
+        encoder_ops=encode_test(test_torch_tensor)
+        print(encode_test)
         #testing the FusionModule part 
         fuse_test=FusionModule(fuse_embed_size=3,class_size=2)
+        #summary(encode_test,(1,3))
         fuse_test=fuse_test.to(device)
-        class_op=fuse_test.forward(encoder_ops,test_image_tensor)
-        print(class_op)
+        class_op=fuse_test(encoder_ops,test_image_tensor)
+        print(class_op.size())
 
 
--- vqa_main.py
+++ vqa_main.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 from dataset_vqa import Dictionary, VQAFeatureDataset
-
+from models import EncoderLSTM, FusionModule
 
 def main(args):
 
@@ -15,6 +15,10 @@ def main(args):
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
     torch.backends.cudnn.benchmark = True
+
+
+    #extract weights from the weight matrices
+    weights=np.load(args.file_name)
 
     # CUDA for PyTorch
     use_cuda = torch.cuda.is_available()
@@ -24,41 +28,62 @@ def main(args):
     dictionary = Dictionary.load_from_file('data/dictionary.pkl')
     train_dataset = VQAFeatureDataset('train', dictionary)
     eval_dataset = VQAFeatureDataset('val', dictionary)
-    batch_size = args.batch_size
+    
 
     #model definition 
+    question_encoder=EncoderLSTM(hidden_size=args.num_hid,weights_matrix=weights,fc_size=args.q_embed,max_seq_length=args.max_sequence_length,batch_size=args.batch_size).to(device)
+    fusion_network=FusionModule(fuse_embed_size=args.q_embed,fc_size=args.fuse_embed).to(device)
 
     #Dataloader initialization
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=1)
-    eval_loader =  DataLoader(eval_dataset, batch_size, shuffle=True, num_workers=1)
+    train_loader = DataLoader(train_dataset, args.batch_size, shuffle=True, num_workers=1)
+    eval_loader =  DataLoader(eval_dataset, args.batch_size, shuffle=True, num_workers=1)
 
-    features, spatials, question, target = next(iter(train_loader))
-    #(type(features))
-    #print(type(spatials))
-    #print(type(question))
-    #print(type(target))
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    params = list(question_encoder.parameters()) + list(fusion_network.parameters()) 
+    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
 
-    feats=features.numpy()
-    spatials=spatials.numpy()
-    question=question.numpy()
-    target=target.numpy()
-    print(feats.shape)
-    print(question.shape)
-    print(question[0])
-    print(target)
+    # Train the models
+    total_step = len(train_loader)
 
+    #Training starts
+    for epoch in range(args.epochs):
+        for i, (image_features,spatials,question_tokens,labels) in enumerate(train_loader):
+
+            image_feats=torch.mean(image_features,dim=1)
+            image_feats=image_feats.to(device)
+            question_tokens=question_tokens.to(device)
+
+            #Forward, Backward and Optimize
+            question_features=question_encoder(question_tokens)
+            class_outputs=fusion_network(question_features,image_feats)
+
+            loss = criterion(class_outputs, labels)
+            decoder.zero_grad()
+            encoder.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                      .format(epoch, args.num_epochs, i, total_step, loss.item())) 
     
-    
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=30)
-    parser.add_argument('--num_hid', type=int, default=1024)
-    parser.add_argument('--model', type=str, default='baseline0_newatt')
-    parser.add_argument('--output', type=str, default='saved_models/exp0')
-    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--num_hid', type=int, default=512)
+    #parser.add_argument('--model', type=str, default='baseline0_newatt')
+    parser.add_argument('--file_name', type=str, default="data/glove6b_init_300d.npy")
+    parser.add_argument('--output', type=str, default='saved_models')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--max_sequence_length', type=int, default=14)
     parser.add_argument('--seed', type=int, default=1111, help='random seed')
+    parser.add_argument('--q_embed',type=int, default=2048, help='embedding output of the encoder RNN')
+    parser.add_argument('--fuse_embed',type=int, default=1024, help='Overall embedding size of the fused network')
+    parser.add_argument('--num_class',type=int, default=3125, help='Number of output classes')
+    parser.add_argument('--learning_rate',type=float,default=0.001,help='Learning rate')
     args = parser.parse_args()
     main(args)
 
