95024fc349cb929e6f7374d55eb1b0366d95228e
digbose92 <digbose92@gmail.com>
2019-04-13 18:32:49 -0700

    Added the attention dataloader

--- Visual_Attention/vqa_dataset_attention.py
+++ Visual_Attention/vqa_dataset_attention.py
@@ -18,8 +18,7 @@ from models import *
 from tqdm import tqdm
 import time
 import h5py
-
-def _create_entry(img, question, answer):
+def _create_entry(question, answer):
     answer.pop('image_id')
     answer.pop('question_id')
     entry = {
@@ -34,22 +33,25 @@ def _load_dataset(dataroot,name):
         dataroot, 'v2_OpenEnded_mscoco_%s2014_1000_questions.json' % name)
     questions = sorted(json.load(open(question_path))['questions'],
                        key=lambda x: x['question_id'])
-    answer_path = os.path.join(dataroot, '%s_target_yes_no.pkl' % name)
+    answer_path = os.path.join(dataroot, '%s_target_top_1000_ans.pkl' % name)
     answers = cPickle.load(open(answer_path, 'rb'))
     answers = sorted(answers, key=lambda x: x['question_id'])
     utils.assert_eq(len(questions), len(answers))
     entries = []
+    print(len(questions))
+    print(len(answers))
     for question, answer in zip(questions, answers):
         utils.assert_eq(question['question_id'], answer['question_id'])
         utils.assert_eq(question['image_id'], answer['image_id'])
         img_id = question['image_id']
         if(len(answer['scores'])>0):
             entries.append(_create_entry(question, answer))
+    return(entries)
 
 class Dataset_VQA(Dataset):
     """Dataset for VQA applied to the attention case with image features in .hdf5 file 
     """
-    def __init__(self,img_root_dir,feats_data_path,dictionary,dataroot,filename_len=12,file_list_path=None,choice='train',arch_choice='resnet152',layer_option='pool',transform_set=None):
+    def __init__(self,img_root_dir,feats_data_path,dictionary,dataroot,filename_len=12,choice='train',arch_choice='resnet152',layer_option='pool',transform_set=None):
 
         #initializations
         self.data_root=dataroot
@@ -57,7 +59,7 @@ class Dataset_VQA(Dataset):
         self.img_dir=os.path.join(img_root_dir,choice+"2014")
         self.choice=choice
         self.transform=transform_set
-        self.file_list_path=file_list_path
+        
         self.dictionary=dictionary
         self.arch=arch_choice 
         self.layer_option=layer_option 
@@ -81,6 +83,7 @@ class Dataset_VQA(Dataset):
 
         self.entries=_load_dataset(self.data_root,self.choice)
         self.tokenize()
+        self.tensorize()
 
     def tokenize(self, max_length=14):
         """Tokenizes the questions.
@@ -98,21 +101,58 @@ class Dataset_VQA(Dataset):
             utils.assert_eq(len(tokens), max_length)
             entry['q_token'] = tokens
 
+    def tensorize(self):
+        self.features = torch.from_numpy(self.features)
+        #self.spatials = torch.from_numpy(self.spatials)
+
+        for entry in self.entries:
+            question = torch.from_numpy(np.array(entry['q_token']))
+            entry['q_token'] = question
+            answer = entry['answer']
+            class_labels = np.array(answer['Class_Label'])
+            labels=np.array(answer['labels'])
+            scores = np.array(answer['scores'], dtype=np.float32)
+            if len(labels):
+                labels = torch.from_numpy(labels)
+                scores = torch.from_numpy(scores)
+                class_labels=torch.from_numpy(class_labels)
+                entry['answer']['labels'] = labels
+                entry['answer']['scores'] = scores
+                entry['answer']['Class_Label']=class_labels
+            else:
+                entry['answer']['labels'] = None
+                entry['answer']['scores'] = None
+                entry['answer']['Class_Label']=None
+
+
     def __getitem__(self,index):
         entry=self.entries[index]
         #filtering of the labels based on the score
         question=entry['q_token']
         answer_data=entry['answer']
-        label=entry['Class_label']
+        label=answer_data['Class_Label']
         image_id=entry['image_id']
 
         filename='COCO_'+self.choice+'2014_'+str(image_id).zfill(self.filename_len)+'.jpg'
         idx=self.file_list.index(os.path.join(self.img_dir,filename))
-
-        feat=self.features[idx]
-
+        
+        feat=torch.from_numpy(self.features.numpy()[idx])
         return(feat,question,label)
 
+    def __len__(self):
+        return(len(self.entries))
 
 
+if __name__ == "__main__":
+    image_root_dir="/data/digbose92/VQA/COCO"
+    dictionary=Dictionary.load_from_file('../Visual_All/data/dictionary.pkl')
+    feats_data_path="/data/digbose92/VQA/COCO/train_hdf5_COCO/"
+    data_root="/proj/digbose92/VQA/VisualQuestion_VQA/common_resources"
+    train_dataset=Dataset_VQA(img_root_dir=image_root_dir,feats_data_path=feats_data_path,dictionary=dictionary,dataroot=data_root,arch_choice="vgg16",layer_option="embedding")
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=1)
 
+    feat,question,label=next(iter(train_loader))
+
+    print(feat.shape)
+    print(question)
+    print(label)
